8729157e4d7a5781f32ea6348d80f4aa3fcc9c84
iamconnaught <wdonnelly22@gmail.com>
2019-04-11 16:57:05 -0500

    commit 2

--- server.js
+++ server.js
@@ -2,9 +2,17 @@ const express = require('express');
 
 const app = express();
 
-app.get('/greeting/:name', (req,res) =>{
-	res.send("Greetings, " + req.params.name)
-});
+// app.get('/greeting/:name', (req,res) =>{
+// 	res.send("Greetings, " + req.params.name)
+// });
+
+
+
+app.get('/tip/:total/:tipPercentage', (req,res)=>{
+	res.send("Tip is " + (req.params.total / 100) * req.params.tipPercentage)
+})
+
+
 
 app.listen(3001, ()=> {
 	console.log('server is listening to 3001');
