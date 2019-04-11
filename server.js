bbe2c8447be9614999a8a4403b1d54e7fabacb39
iamconnaught <wdonnelly22@gmail.com>
2019-04-11 17:29:02 -0500

    commit 3

--- server.js
+++ server.js
@@ -6,14 +6,18 @@ const app = express();
 // 	res.send("Greetings, " + req.params.name)
 // });
 
+const magicResponses = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
 
 
 app.get('/tip/:total/:tipPercentage', (req,res)=>{
 	res.send("Tip is " + (req.params.total / 100) * req.params.tipPercentage)
 })
 
-
+app.get('/magic/:question', (req,res)=>{
+	res.send(req.params.question + magicResponses[Math.floor(Math.random() * magicResponses.length)])
+})
 
 app.listen(3001, ()=> {
 	console.log('server is listening to 3001');
 })
+
